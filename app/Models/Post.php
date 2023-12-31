087b88e7ef00c78fd3b2b16020b5f9e200f17fa6
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2023-12-31 15:25:52 +0200

    Add relationships between Category and Post models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Post extends Model
 {
@@ -20,9 +21,14 @@ class Post extends Model
     'user_id' 
 ];
 
-public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+public function user(): BelongsTo
 {
     return $this->belongsTo(User::class);
+}
+
+public function categories(): BelongsToMany
+{
+    return $this->belongsToMany(Category::class);
 }
 
 }
