50eee4fbb7002d02b3e5e96eb98263f3473257c7
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2024-01-02 21:33:42 +0200

    Add navigation group to CategoryResource and PostResource

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -12,18 +12,23 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
-use Illuminate\Support\Str; 
+use Illuminate\Support\Str;
+use League\CommonMark\Extension\CommonMark\Node\Block\Heading;
 
 class PostResource extends Resource
 {
     protected static ?string $model = Post::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-clipboard-document-list';
+
+    protected static ?string $navigationGroup = 'Content';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
+
+
                 Forms\Components\TextInput::make('title')
                     ->required()
                     ->maxLength(2048)
