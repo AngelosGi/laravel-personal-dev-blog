e63923f894c5ac03e4c6ee0629494f995104a25b
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2024-01-04 21:56:41 +0200

    Add getShortBodyAttribute method to Post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Str;
 
 class Post extends Model
 {
@@ -30,5 +31,11 @@ public function categories(): BelongsToMany
 {
     return $this->belongsToMany(Category::class);
 }
+
+public function getShortBodyAttribute(): string
+{
+    return Str::words(strip_tags($this->body), 30);
+}
+
 
 }
