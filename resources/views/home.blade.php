c8da990b8af85db4936af4445178859c2c20af05
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2024-01-06 20:17:14 +0200

    Update posts section layout in home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,17 @@
+<?php 
+   /** @var $posts \Illuminate\Pagination\LengthAwarePaginator */
+?>
 <x-app-layout>
+
 <!-- Posts Section -->
-    <section class="w-full md:w-2/3 flex flex-col items-center px-3">
+    <section class="w-full md:w-2/4 flex flex-col items-center px-3">
 
     @foreach ($posts as $post)
     <x-post-item :post="$post"></x-post-item>
     @endforeach
 
-    {{$posts->links()}}
+   
+    {{$posts->onEachSide(1)->links()}}
 
     </section>
 
