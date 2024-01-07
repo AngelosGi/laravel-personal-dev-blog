873a833adf42d7fdca5a401a9df3973d9fc44b2a
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2024-01-07 22:48:59 +0200

    Chore Update sidebar content and button text

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -13,10 +13,10 @@
 
 
     <div class="w-full bg-white flex flex-col my-4 p-6">
-        <p class="text-xl font-semibold pb-5">{{ \App\Models\TextWidget::getTitle('about-us-sidebar') }}</p>
-        {!! \App\Models\TextWidget::getContent('about-us-sidebar') !!}
+        <p class="text-xl font-semibold pb-5">{{ \App\Models\TextWidget::getTitle('about-me-sidebar') }}</p>
+        {!! \App\Models\TextWidget::getContent('about-me-sidebar') !!}
         <a href="#" class="w-full bg-blue-800 text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-4">
-            Get to know us
+            Get in contact!
         </a>
     </div>
 
